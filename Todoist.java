d28fb55dbdb6bdb81e8a82d1699fe321a70ea56e
Dídac Fernández Fernández <forcriun@gmail.com>
2017-11-24 13:01:13 +0100

    Pre-demostracion expresiones en returns

---
+++ Todoist.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+
+
+/**
+ * Clase que permite almacenar las tareas pendientes que tenemos por hacer.
+ */
+public class Todoist 
+{
+    
+    // Almacena nuestras tareas
+    private ArrayList<String> tareas; 
+
+    /**
+     * Constructor de la clase Todoist. Crea un gestor con las 3 tareas vacías.
+     */
+    public Todoist()
+    {
+        tareas = new ArrayList<String>();
+    }
+    
+    
+    /**
+     * Inserta una nueva tarea
+     */
+    public void addTarea(String nombreTarea)
+    {
+        tareas.add(nombreTarea);
+    }
+    
+    
+    /**
+     * Muestra por pantalla todas las tareas existentes
+     */
+    
+    public void mostrarTareas()
+    {
+        System.out.println("Tareas existentes:");
+        System.out.println(tareas);
+    }
+    
+    
+    /**
+     * Devuelve el numero de tareas pendientes
+     */     
+    public int getNumeroDeTareasPendientes()
+    {
+      return tareas.size(); 
+    }
+    
+    
+    /**
+     * Imprime por pantalla el numero de tareas pendientes
+     */
+    public void mostrarNumeroTareasPendientes()
+    {
+        System.out.println(tareas.size());
+    }
+    
+    
+    /** 
+     * Elimina la tarea que ocupa la posicion indicada como
+     * parametro (empezando en 0). Devuelve true si la tarea existe y se elimina,
+     * false en caso contrario
+     */
+    public boolean eliminarTarea(int posicionTarea) 
+    {
+        boolean valorADevolver = false;
+        if(posicionTarea >= 0 && posicionTarea < tareas.size()){
+            tareas.remove(posicionTarea);
+            valorADevolver = true;
+        }
+        return valorADevolver;
+    }
+    
+}
+
